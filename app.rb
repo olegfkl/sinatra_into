69443428b398437bfceec8aaf85d445f9d0bb69f
Marco Vanali <Marco91Vanali@gmail.com>
2017-11-07 11:48:01 +0000

    Interpolated a ruby syntax inside an html tag

--- app.rb
+++ app.rb
@@ -1,15 +1,15 @@
 require 'sinatra'
 
-# get '/' do
-#   "Hello"
-# end
-#
-#
-# get '/secret' do
-#   "I love nature"
-# end
+get '/' do
+  "Hello"
+end
+
+
+get '/secret' do
+  "I love nature"
+end
 
 
 get '/cat' do
-  "erb(:index)"
+  erb(:index)
 end
