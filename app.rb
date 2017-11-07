514dca3776bb9bb35a498aafff5b3c9550bd63e4
Oleg Grushetskyy <olegfkl@gmail.com>
2017-11-07 11:23:52 +0000

    Add shotgun to Gemfile

--- app.rb
+++ app.rb
@@ -1,5 +1,20 @@
 require 'sinatra'
 
 get '/' do
-  "Hello World"
+  "Hello"
+end
+
+
+get '/secret' do
+  "I love nature"
+end
+
+# get '/:name' do |n|
+#   "I am  #{n}"
+# end
+
+get '/cat' do
+
+  '<img style="border: medium dashed red" src="http://f.cl.ly/items/0k0v3e2X3l2f3i1n1Y19/Screen%20Shot%202013-09-10%20at%2011.32.00.png"/>'
+
 end
